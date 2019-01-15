300bc76024f4150b27d370d26d62f99bd2c59e78
rak3me <rak3me@virginia.edu>
2019-01-15 11:57:42 -0500

    Editor tools for Post-Processing. Moving files

---
+++ Dillow/Assets/PostProcessingVolume/Editor/PostProcessingEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.PostProcessing;
+
+public class PostProcessingEditor : MonoBehaviour {
+
+	public static bool active = false;
+
+	[MenuItem("Post-Processing/Toggle %#t")]
+	public static void TogglePostProcessing () {
+		foreach (PostProcessingBehaviour behavior in FindObjectsOfType<PostProcessingBehaviour>()) {
+			behavior.enabled = active;
+		}
+
+		active = !active;
+	}
+}
