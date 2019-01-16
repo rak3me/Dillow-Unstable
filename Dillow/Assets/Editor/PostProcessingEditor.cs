3d580a0a60fba6ac989fc1e7df18a388c6ca4613
rak3me <rak3me@virginia.edu>
2019-01-15 19:11:55 -0500

    Gaia map test

---
+++ Dillow/Assets/Editor/PostProcessingEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessingEditor : MonoBehaviour {
+
+	public static bool active = false;
+
+	[MenuItem("Post-Processing/Toggle %#t")]
+	public static void Toggle () {
+		FindObjectOfType<PostProcessLayer>().enabled = active;
+
+		active = !active;
+	}
+}
